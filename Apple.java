cca3f4eff418a376ae5c3c3714981a103a30944b
fishKoKo <49087865+fishKoKo@users.noreply.github.com>
2019-05-26 21:03:31 +0800

    新特性

---
+++ Apple.java
@@ -0,0 +1,18 @@
+package newtexing;
+
+public class Apple {
+
+	private String color;
+	private double weight;
+	
+	public Apple(String str, int i) {
+		// TODO 自动生成的构造函数存根
+		this.color=str;
+		this.weight=i;
+	}
+
+	    public String toString() {
+	        return "color:" + color + "	weight:" + weight;
+	    }
+
+}
